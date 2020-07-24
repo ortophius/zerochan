58d80659ddd6b25dfc3efe51dcf928b939331d91
ortophius@mail.ru <ortophius@mail.ru>
2020-07-24 20:20:10 +0400

    eventBus done

--- src/modules/Bus.js
+++ src/modules/Bus.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid').v4;
+
 class Listener {
     constructor(callback, once) {
         this.callback = callback;
@@ -9,34 +11,39 @@ const Bus = {
     subscriptions: {},
     subscribe: function(eventType, callback, once) {
         const _ = this;
-        const id = Date.now();
+        const id = uuid();
+        once = (once === undefined) ? false : once;
 
         // Some dumb checks
-        if (typeof eventType != "string") return false;
-        if (!(callback instanceof Object)) return false;
+        if (typeof eventType != 'string') return false;
+        if (typeof callback != 'function') return false;
 
-        subscription[eventType][id] = new Listener(callback, once);
-        return { unsubscribe: _.unsubscribe.bind(eventType, id) }
+        if (this.subscriptions[eventType] === undefined) this.subscriptions[eventType] = {};
+        this.subscriptions[eventType][id] = new Listener(callback, once);
+        return {
+            id: id,
+            unsubscribe: _.unsubscribe.bind(_, eventType, id),
+        }
     },
     unsubscribe: function(eventType, id){
-        delete subscriptions[eventType][id];
-        if (Object.keys(subscriptions[eventType]) === 0)
-            delete subscriptions[eventType];
+        delete this.subscriptions[eventType][id];
+        if (Object.keys(this.subscriptions[eventType]).length === 0)
+            delete this.subscriptions[eventType];
         return id;
     },
-    emit: function(e){
-        if (!(e instanceof Event)) return false;
-        if (this[e.eventType] === undefined) return false;
+    emit: function(eventType, data){
+        if (typeof(eventType) !== 'string') return false;
+        if (this.subscriptions[eventType] === undefined) return false;
 
-        const events = this[e.eventType];
+        const events = this.subscriptions[eventType];
         const _ = this;
         
         Object.keys(events).forEach(function(id){
             const listener = events[id];
-            listener.callback();
+            listener.callback(data);
             if (listener.once) _.unsubscribe(eventType, id);
         });
     }
 };
 
-exports.default = Bus;
+module.exports = Bus;
--- src/tests/Bus.test.js
+++ src/tests/Bus.test.js
@@ -1,5 +1,56 @@
+const should = require('chai').should();
 const Bus = require('../modules/Bus');
-const assert = require('assert');
 
 describe("Bus", function(){
+    const cb = function(){};
+    let sharedSub;
+
+    it('Register listener', function(){
+        sharedSub = Bus.subscribe('test', cb);
+        Bus.subscriptions.should.have.property('test');
+        Bus.subscriptions.test.should.have.property(sharedSub.id);
+    });
+
+    it('Unsubscribe properly', function(){     
+        const id = sharedSub.id;
+        sharedSub.unsubscribe();
+        Bus.subscriptions.should.not.have.property('test');  
+    });
+
+    it('Register multiple liteners', function(){
+        const sub1 = Bus.subscribe('multitest', cb);
+        const sub2 = Bus.subscribe('multitest', cb);
+
+        const listenersObj = Bus.subscriptions.multitest;
+        Object.keys(listenersObj).length.should.equal(2);
+        sub1.unsubscribe();
+        sub2.unsubscribe();
+    });
+
+    it('Emitting events', function(done){
+        sub = Bus.subscribe('emittest', done);
+        Bus.emit('emittest');
+        sub.unsubscribe();
+    });
+
+    it('Passing data', function(done){
+        const dataToPass = 'test';
+        let checkFunc = function(passedData) {
+            if (passedData == dataToPass) done()
+            else {
+                console.log(passedData);
+                return false;
+            }
+        };
+
+        sub = Bus.subscribe('dataTest', checkFunc);
+        Bus.emit('dataTest', 'test')
+        sub.unsubscribe();
+    });
+
+    it('Automatically unsub listeners with \'once\' parameter', function(){
+        const sub = Bus.subscribe('onceTest', cb, true);
+        Bus.emit('onceTest');
+        Bus.subscriptions.should.not.have.property('onceTest');
+    });
 });
